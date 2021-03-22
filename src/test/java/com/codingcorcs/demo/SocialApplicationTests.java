146bae0a9b533b09f41f3ea20e29e7b2888b93fe
andrewcomputsci2019 <54915639+andrewcomputsci2019@users.noreply.github.com>
2021-03-22 08:35:11 -0700

    first commit

---
+++ src/test/java/com/codingcorcs/demo/SocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingcorcs.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
