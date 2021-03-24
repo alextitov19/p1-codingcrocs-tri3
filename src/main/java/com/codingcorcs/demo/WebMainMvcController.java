ea8e750cb606a96ac50e024438a932d3fcfc1724
andrewcomputsci2019 <54915639+andrewcomputsci2019@users.noreply.github.com>
2021-03-24 11:24:40 -0700

    added some comments for the main controller added A main Side Bar for the instagram project

--- src/main/java/com/codingcorcs/demo/WebMainMvcController.java
+++ src/main/java/com/codingcorcs/demo/WebMainMvcController.java
@@ -3,6 +3,10 @@ package com.codingcorcs.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * Main controller of the project will handle the landing page and things related to it
+ * @see SocialApplication
+ */
 @Controller
 public class WebMainMvcController {
     @GetMapping("/hello")
