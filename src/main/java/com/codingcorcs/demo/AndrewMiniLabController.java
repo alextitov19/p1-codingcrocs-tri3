5ce6e83e72eb78eeec962e0a7e2576979a653d0a
andrewcomputsci2019 <54915639+andrewcomputsci2019@users.noreply.github.com>
2021-04-02 15:48:30 -0700

    added a second mapping and default value for path variable

--- src/main/java/com/codingcorcs/demo/AndrewMiniLabController.java
+++ src/main/java/com/codingcorcs/demo/AndrewMiniLabController.java
@@ -2,21 +2,24 @@ package com.codingcorcs.demo;
 
 import com.codingcorcs.demo.MiniLabs.Andrew.Recursion.Fibo;
 import com.codingcorcs.demo.MiniLabs.Andrew.Recursion.Recursion;
+import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Optional;
+
 @Controller
 @RequestMapping({"/miniLab/Andrew","/minilab/Andrew"})
 public class AndrewMiniLabController {
-    @GetMapping("/Fib/{nth}")
-    @ResponseBody
-    public String Tester(@PathVariable int nth)
+    @GetMapping({"/Fib/{nth}","/Fib/"})
+    @ResponseBody // restfull return
+    public String Tester(@PathVariable Optional<Integer> nth)
     {
-        return new Recursion().getFib(nth).toString();
-    }
+        return new Recursion().getFib(nth.orElse(0)).toString();
+    } //returns json list
 
 
 }
