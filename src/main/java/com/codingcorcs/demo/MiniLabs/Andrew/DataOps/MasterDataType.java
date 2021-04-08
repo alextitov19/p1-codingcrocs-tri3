cdfa92e47a77c7a24f4ac29fb2b1849c50c01882
andrewcomputsci2019 <54915639+andrewcomputsci2019@users.noreply.github.com>
2021-04-07 18:27:46 -0700

    Started Work on Data Ops mini Lab and Have Abstract Classes setUp

---
+++ src/main/java/com/codingcorcs/demo/MiniLabs/Andrew/DataOps/MasterDataType.java
@@ -0,0 +1,9 @@
+package com.codingcorcs.demo.MiniLabs.Andrew.DataOps;
+
+public abstract class MasterDataType {
+    public final String masterType = "MasterDataType";
+    public String type;
+
+    @Override
+    public abstract String toString();
+}
