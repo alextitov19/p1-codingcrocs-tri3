7b85eb23fd4403aec5b1cfd751c7d164c5c0dff4
Jett Kim <jettkim1@gmail.com>
2021-04-09 22:08:47 -0700

    Insertion Sort Commit

--- src/main/java/com/codingcorcs/demo/JettMiniLabController.java
+++ src/main/java/com/codingcorcs/demo/JettMiniLabController.java
@@ -1,11 +1,15 @@
 package com.codingcorcs.demo;
 
+import com.codingcorcs.demo.MiniLabs.Jett.InsertionRecursion;
 import com.codingcorcs.demo.MiniLabs.Jett.PalindromeCheck;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Arrays;
+import java.util.Random;
 
 @Controller
 @RequestMapping({"/miniLab/Jett","/minilab/Jett"})
@@ -20,4 +24,20 @@ public class JettMiniLabController {
         }
         return "Jett/JettPalindrome";
     }
+
+    @GetMapping("/insertion")
+    public String InsertionTemplate(Model model)
+    {
+        Random rand = new Random();
+        int[] arr = new int[10];
+        for(int i=0;i<arr.length;i++){
+            arr[i] = rand.nextInt(100)+1;
+        }
+        InsertionRecursion ir = new InsertionRecursion();
+        int[] sr = ir.insertionSort(arr);
+        model.addAttribute("sorted", Arrays.toString(sr));
+        model.addAttribute("unsorted", Arrays.toString(arr));
+        return "Jett/JettInsertion";
+    }
+
 }
