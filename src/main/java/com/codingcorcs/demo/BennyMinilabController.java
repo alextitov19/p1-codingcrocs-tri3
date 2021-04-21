cef8ca026747b1f2e309bd6b538b6f2c89c32a57
bennyherrick <54915537+bennyherrick@users.noreply.github.com>
2021-04-21 14:22:32 -0700

    new

--- src/main/java/com/codingcorcs/demo/BennyMinilabController.java
+++ src/main/java/com/codingcorcs/demo/BennyMinilabController.java
@@ -1,19 +1,36 @@
 package com.codingcorcs.demo;
 
+import com.codingcorcs.demo.MiniLabs.Jett.InsertionRecursion;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Arrays;
+import java.util.Random;
+
 @Controller
 @RequestMapping({"/miniLab/Recursion","/minilab/Recursion"})
 public class BennyMinilabController {
 
     @GetMapping("/Recursion")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("recursion", name);
         return "Bennyrec";
     }
 
+    @GetMapping("/insertion")
+    public String InsertionTemplate(Model model) {
+        Random rand = new Random();
+        int[] arr = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rand.nextInt(100) + 1;
+        }
+        InsertionRecursion ir = new InsertionRecursion();
+        int[] sr = ir.insertionSort(arr);
+        model.addAttribute("sorted", Arrays.toString(sr));
+        model.addAttribute("unsorted", Arrays.toString(arr));
+        return "Bennysorts";
+    }
 }
