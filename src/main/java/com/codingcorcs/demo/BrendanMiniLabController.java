83200d69aac3753feee9776d006ed7168c86a7af
brendan8503 <72753567+brendan8503@users.noreply.github.com>
2021-04-27 16:00:57 -0700

    Update BrendanMiniLabController.java

--- src/main/java/com/codingcorcs/demo/BrendanMiniLabController.java
+++ src/main/java/com/codingcorcs/demo/BrendanMiniLabController.java
@@ -11,7 +11,31 @@ public class BrendanMiniLabController {
     @GetMapping("/Brendan")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
-        return "BrendanT";
+        return "Image";
+
+        
+@Controller
+@RequestMapping({"/miniLab/Brendan","/minilab/Brendan"})
+public class BrendanMinilabController {
+
+    @GetMapping("/Recursion")
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("recursion", name);
+        return "Brendanrec";
     }
 
-}
+    @GetMapping("/Inheritance")
+    public String greeting2(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("Inheritance", name);
+        return "Brendanint";
+    }
+
+    @GetMapping("/Sorts")
+    public String greeting3(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("Sorts", name);
+        return "Brendansorts";
+    }
+} 
+   
+
+
