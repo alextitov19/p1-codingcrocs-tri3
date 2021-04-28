8e4057ac653e58b3438f396d8b8ddc0c13bb33d2
andrewcomputsci2019 <54915639+andrewcomputsci2019@users.noreply.github.com>
2021-04-27 19:47:50 -0700

    Fixed Bredan Code

--- src/main/java/com/codingcorcs/demo/BrendanMiniLabController.java
+++ src/main/java/com/codingcorcs/demo/BrendanMiniLabController.java
@@ -3,6 +3,7 @@ package com.codingcorcs.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -12,30 +13,32 @@ public class BrendanMiniLabController {
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "Image";
-
-        
-@Controller
-@RequestMapping({"/miniLab/Brendan","/minilab/Brendan"})
-public class BrendanMinilabController {
-
-    @GetMapping("/Recursion")
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
-        model.addAttribute("recursion", name);
-        return "Brendanrec";
     }
 
-    @GetMapping("/Inheritance")
-    public String greeting2(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
-        model.addAttribute("Inheritance", name);
-        return "Brendanint";
-    }
 
-    @GetMapping("/Sorts")
-    public String greeting3(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
-        model.addAttribute("Sorts", name);
-        return "Brendansorts";
+        @Controller
+        @RequestMapping({"/miniLab/Brendan", "/minilab/Brendan"})
+        class BrendanMinilabController {
+
+            @GetMapping("/Recursion")
+            public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+                model.addAttribute("recursion", name);
+                return "Brendanrec";
+            }
+
+            @GetMapping("/Inheritance")
+            public String greeting2(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+                model.addAttribute("Inheritance", name);
+                return "Brendanint";
+            }
+
+            @GetMapping("/Sorts")
+            public String greeting3(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+                model.addAttribute("Sorts", name);
+                return "Brendansorts";
+            }
+        }
     }
-} 
    
 
 
