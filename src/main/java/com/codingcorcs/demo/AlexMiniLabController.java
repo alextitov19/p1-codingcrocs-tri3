7a8d98daf0c46dc2cecc1432b4dbcecc98241a0b
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2021-05-02 16:51:42 -0700

    update

--- src/main/java/com/codingcorcs/demo/AlexMiniLabController.java
+++ src/main/java/com/codingcorcs/demo/AlexMiniLabController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class AlexMiniLabController {
 
-    @GetMapping("/alex")
+    @GetMapping("/alexFirstLab")
     public String index(Model model) {
         return "Alex";
     }
