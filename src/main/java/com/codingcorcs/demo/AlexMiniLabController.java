cec764cab5dad543af1557838f27ca0b1694de06
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2021-05-02 16:55:26 -0700

    recursion

--- src/main/java/com/codingcorcs/demo/AlexMiniLabController.java
+++ src/main/java/com/codingcorcs/demo/AlexMiniLabController.java
@@ -3,14 +3,13 @@ package com.codingcorcs.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AlexMiniLabController {
 
     @GetMapping("/alexFirstLab")
     public String index(Model model) {
-        return "Alex";
+        return "Alex/Recursion";
     }
 
 }
