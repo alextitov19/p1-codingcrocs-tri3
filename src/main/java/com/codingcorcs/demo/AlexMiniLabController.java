d775ea1049085476140173ded52cdab2a32398ed
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2021-05-02 17:03:43 -0700

    moved labs

--- src/main/java/com/codingcorcs/demo/AlexMiniLabController.java
+++ src/main/java/com/codingcorcs/demo/AlexMiniLabController.java
@@ -7,9 +7,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AlexMiniLabController {
 
-    @GetMapping("/alex/FirstLab")
-    public String index(Model model) {
+    @GetMapping("/alex/recursion")
+    public String recursionIndex(Model model) {
         return "Alex/Recursion";
+    }
+
+    @GetMapping("/alex/inheritance")
+    public String inheritanceIndex(Model model) {
+        return "Alex/Inheritance";
+    }
+
+    @GetMapping("/alex/sorting")
+    public String sortingIndex(Model model) {
+        return "Alex/Sorting";
+    }
+
+    @GetMapping("/alex/linkedlist")
+    public String linkedListIndex(Model model) {
+        return "Alex/LinkedList";
     }
 
 }
