ff5757b880aaa0ed5173da7dd853c688679d4f2d
andrewcomputsci2019 <54915639+andrewcomputsci2019@users.noreply.github.com>
2021-05-19 16:10:01 -0700

    added postgres database dependency and added Mappings for SignUp page

--- src/main/java/com/codingcorcs/demo/WebMainMvcController.java
+++ src/main/java/com/codingcorcs/demo/WebMainMvcController.java
@@ -2,6 +2,7 @@ package com.codingcorcs.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * Main controller of the project will handle the landing page and things related to it
@@ -19,5 +20,15 @@ public class WebMainMvcController {
     {
         return "Hello"; // place holder for now
     }
+
+    @GetMapping("/SignUp")
+    public String SignUpPage(){
+        return null; // place holder
+    }
+    @PostMapping("/SignUp")
+    public String SignUpPage(Object obj){
+        return null; //place holder
+    }
+
 
 }
