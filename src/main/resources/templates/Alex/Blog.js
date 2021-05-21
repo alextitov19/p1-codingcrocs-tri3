286619466fe7769210d0467e801abe1b0d0c78cb
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2021-05-20 17:15:03 -0700

    added firebase

--- src/main/resources/templates/Alex/Blog.js
+++ src/main/resources/templates/Alex/Blog.js
@@ -1,8 +1,25 @@
+
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import firebase from "firebase/app";
+// If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
+// import * as firebase from "firebase/app"
+
+
 var mainText = document.getElementById("mainText");
 var submitButton = document.getElementById("submitButton");
 
 function submitClick() {
 
-    window.alert("OKAY")
+    // Get a reference to the root of the Database
+    var database = firebase.database();
+    writeUserData("someID", "someName", "someEmail", "someURL")
 
+}
+
+function writeUserData(userId, name, email, imageUrl) {
+    firebase.database().ref('users/' + userId).set({
+        username: name,
+        email: email,
+        profile_picture : imageUrl
+    });
 }
