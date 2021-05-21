4631ed89d048e7edcc90aa80166013fd513985c7
andrewcomputsci2019 <54915639+andrewcomputsci2019@users.noreply.github.com>
2021-05-20 23:47:14 -0700

    optimized imports

--- src/main/java/com/codingcorcs/demo/WebMainMvcController.java
+++ src/main/java/com/codingcorcs/demo/WebMainMvcController.java
@@ -1,14 +1,12 @@
 package com.codingcorcs.demo;
 
 import com.codingcorcs.demo.NewUser.NewUser;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
 import java.sql.Connection;
