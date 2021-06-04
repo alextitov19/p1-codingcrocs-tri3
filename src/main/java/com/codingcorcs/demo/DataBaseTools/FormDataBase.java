a8baf75fe39a0f80076b09ea8bf9fa0f943b37f4
andrewcomputsci2019 <54915639+andrewcomputsci2019@users.noreply.github.com>
2021-06-04 00:42:15 -0700

    added database support for query of forms and related data

--- src/main/java/com/codingcorcs/demo/DataBaseTools/FormDataBase.java
+++ src/main/java/com/codingcorcs/demo/DataBaseTools/FormDataBase.java
@@ -1,4 +1,79 @@
 package com.codingcorcs.demo.DataBaseTools;
 
+import com.codingcorcs.demo.Dto.Comment;
+import com.codingcorcs.demo.Dto.Forms;
+
+import java.sql.*;
+import java.text.Normalizer;
+import java.util.AbstractMap;
+import java.util.ArrayList;
+
 public class FormDataBase {
+
+
+    /**
+     * Should have learned jpa im crying
+     * @return all titles from database
+     */
+    public Forms[] formTitles(){
+        String sqlStatement = "SELECT post_id, form_title FROM forms LIMIT 90";
+        try(Connection connection = DriverManager.getConnection(Config.getUrlForm(),Config.getUser(),Config.getPassword())){
+            Statement statement = connection.createStatement();
+           ResultSet set = statement.executeQuery(sqlStatement);
+            ArrayList<Forms> forms = new ArrayList<>();
+            /*
+                result set should contain
+                post_id
+
+             */
+           while (set.next()){
+                    forms.add(new Forms(set.getLong("post_id"), set.getString("form_title")));
+           }
+           return forms.toArray(new Forms[0]);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public AbstractMap.SimpleEntry<ArrayList<Forms>,ArrayList<Comment>> getForm(long post_id)  {
+        ArrayList<Forms> form = new ArrayList<>(); // async
+        ArrayList<Comment> comment = new ArrayList<>();
+      Thread formThread =  new Thread(()->{
+            String sql = String.format("SELECT * from forms where forms.post_id=%d",post_id);
+            try(Connection connection = DriverManager.getConnection(Config.getUrlForm(),Config.getUser(),Config.getPassword())){
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery(sql);
+                if (resultSet.next()){
+                      form.add(new Forms(resultSet.getLong("post_id"),resultSet.getString("form_title"),resultSet.getString("poster_name"),resultSet.getString("post_content")));
+                }
+            }catch (SQLException e){
+                e.printStackTrace();
+            }
+        });
+      Thread commentThread = new Thread(() ->{
+                String sqlStatment = String.format("SELECT * from comment where comment.post_id=%d",post_id);
+                try(Connection connection = DriverManager.getConnection(Config.getUrlForm(),Config.getUser(),Config.getPassword())){
+                    Statement statement = connection.createStatement();
+                    ResultSet set = statement.executeQuery(sqlStatment);
+                    while(set.next()){
+                        comment.add(new Comment(set.getLong("comment_id"),set.getLong("post_id"),set.getString("poster_name"),set.getString("content_text"),set.getBoolean("reply"),set.getString("reply_user")));
+                    }
+                }catch (SQLException e){
+                    e.printStackTrace();
+                }
+      });
+      formThread.start();
+      commentThread.start();
+      try {
+          formThread.join();
+          commentThread.join(); // joins the threads together to wait for but result sets to be process
+      }catch (InterruptedException e) {
+          e.printStackTrace();
+          return null;
+      }
+      return new AbstractMap.SimpleEntry<>(form,comment);
+    }
+
 }
---
+++ src/main/java/com/codingcorcs/demo/Dto/Comment.java
@@ -0,0 +1,68 @@
+package com.codingcorcs.demo.Dto;
+
+public class Comment {
+    private Long comment_id;
+    private Long post_id;
+    private String poster_name;
+    private String content_text;
+    private boolean reply;
+    private String reply_user;
+
+    public Comment(Long comment_id, Long post_id, String poster_name, String content_text, boolean reply, String reply_user) {
+        this.comment_id = comment_id;
+        this.post_id = post_id;
+        this.poster_name = poster_name;
+        this.content_text = content_text;
+        this.reply = reply;
+        this.reply_user = reply_user;
+    }
+
+    public Long getComment_id() {
+        return comment_id;
+    }
+
+    public void setComment_id(Long comment_id) {
+        this.comment_id = comment_id;
+    }
+
+    public Long getPost_id() {
+        return post_id;
+    }
+
+    public void setPost_id(Long post_id) {
+        this.post_id = post_id;
+    }
+
+    public String getPoster_name() {
+        return poster_name;
+    }
+
+    public void setPoster_name(String poster_name) {
+        this.poster_name = poster_name;
+    }
+
+    public String getContent_text() {
+        return content_text;
+    }
+
+    public void setContent_text(String content_text) {
+        this.content_text = content_text;
+    }
+
+    public boolean isReply() {
+        return reply;
+    }
+
+    public void setReply(boolean reply) {
+        this.reply = reply;
+    }
+
+    public String getReply_user() {
+        return reply_user;
+    }
+
+    public void setReply_user(String reply_user) {
+        this.reply_user = reply_user;
+    }
+}
+
